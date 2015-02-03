c30806c7968571fdbe6fe42258df71539538fdf6
Becky Hamm <becky@metricslice.com>
2015-02-03 13:29:46 -0500

    initial commit

---
+++ normal.js
@@ -0,0 +1,114 @@
+// Below is a series of functions, each function has a
+// description before it that explains what each of them
+// should do. Complete the function so that it satisfies
+// it's description.
+
+
+
+// 1.
+// Should always return the value 5
+
+function giveMeFive() {
+    return 5;
+
+}
+
+giveMeFive();
+
+//---------
+
+// 2.
+// This function should accept 1 parameter
+// and return the exact same thing. This type
+// of function is called a no-op as in (no operation)
+
+function noOp(param) {
+    return param;
+
+}
+
+noOp("thing");
+
+//---------
+
+// 3.
+// This function should accept 1 parameter; a number.
+// It should return the number doubled. Doubled meaning
+// it should return a number twice the size as the number
+// that is passed in.
+
+function double() {
+
+}
+
+//---------
+
+// 4.
+// This function should accept 1 parameter; a number.
+// It should return the number squared. Squared meaning
+// it should return the number multiplied by itself.
+
+function square() {
+
+}
+
+//---------
+
+// 5.
+// This function should accept 1 parameter; an array
+// of numbers. It should calculate the sum (the total
+// of all the numbers in the array added together)
+// and return it.
+
+function sum() {
+
+}
+
+//---------
+
+// 6.
+// This function should accept 1 parameter; a single
+// letter string (like "g".) It should return the index
+// of that letter in the alphabet. For example "a" would
+// be 1 and "z" would be 26. This should work for both
+// lowercase and capital letters.
+
+// HINT: use an array and a for loop
+
+function letterIndex() {
+
+}
+
+//---------
+
+// 7.
+// This function should accept 1 parameter; a number.
+// It should return the letter that corresponds with
+// that number. For example, 1 would be "a" and 26
+// would be "z". This is the inverse of the previous
+// function.
+
+// Note: this needs to work for numbers larger than 26.
+// For example 27 should loop back around to "a".
+
+// HINT: use an array and the modulo (%) operator
+
+function reverseLetterIndex() {
+
+}
+
+//---------
+
+// 8.
+// This function should accept 1 parameter; a string.
+// It should shift every letter in the string by 13
+// places. This is a form of Caesar cipher known as
+// ROT-13. If you are unsure of exactly what you are
+// supposed to do read the wikipedia article that explains
+// it quite well: http://en.wikipedia.org/wiki/ROT13
+
+// HINT: use the previous 2 functions
+
+function rot13() {
+
+}
